1692eb692f13e412c1ced0fa7cabe9572ce33dc5
KimSeongHwan <89847975+PotatoMeme@users.noreply.github.com>
2024-04-10 09:31:09 +0900

    [level 0] Title: 부분 문자열, Time: 8.87 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.kt
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(str1: String, str2: String): Int {
-        loop@ for (index2 in 0..str2.length - str1.length) {
-            for ((index1, c1) in str1.withIndex()) {
-                if (c1 != str2[index2 + index1]) continue@loop
-            }
-            return 1
-        }
-        return 0
-    }
+    fun solution(str1: String, str2: String): Int = if (str1 in str2) 1 else 0
 }
