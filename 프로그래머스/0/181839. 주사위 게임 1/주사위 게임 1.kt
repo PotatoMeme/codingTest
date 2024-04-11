46e8842c8a003edfbd699fc890fb2898e8dfb101
KimSeongHwan <89847975+PotatoMeme@users.noreply.github.com>
2024-04-12 08:37:46 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.kt
@@ -0,0 +1,5 @@
+import kotlin.math.abs
+class Solution {
+    fun solution(a: Int, b: Int) =
+        if (a % 2 == 1 && b % 2 == 1) a * a + b * b else if (a % 2 == 1 || b % 2 == 1) 2 * (a + b) else abs(a - b)
+}
