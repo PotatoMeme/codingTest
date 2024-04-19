b84d6cd80506569f781c42dbca39b227679a628d
KimSeongHwan <89847975+PotatoMeme@users.noreply.github.com>
2024-04-19 09:48:36 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 64.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(a: Int, b: Int, n: Int): Int {
+        var answer: Int = n / a * b
+        return answer + (answer + n % a).let {
+            if (it < a) 0 else solution(a, b, it)
+        }
+    }
+}
