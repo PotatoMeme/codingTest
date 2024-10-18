22fa8148a2cf33e59c435424c5786aa45a3b2047
KimSeongHwan <89847975+PotatoMeme@users.noreply.github.com>
2024-10-18 22:46:56 +0900

    [Silver III] Title: 시리얼 번호, Time: 84 ms, Memory: 13156 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.kt
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.kt
@@ -1,19 +1,15 @@
+import java.util.Arrays
 fun main() = with(System.`in`.bufferedReader()) {
     val arr: Array<String> = Array(readLine().toInt()) {
         readLine()
     }
-    arr.sortWith { a, b ->
-        if (a.length > b.length)
-            1
-        else if (a.length < b.length)
-            -1
-        else if (a.sumOf { it.digitToIntOrNull() ?: 0 } > b.sumOf { it.digitToIntOrNull() ?: 0 })
-            1
-        else if (a.sumOf { it.digitToIntOrNull() ?: 0 } < b.sumOf { it.digitToIntOrNull() ?: 0 })
-            -1
-        else
-            a.compareTo(b)
-    }
+    Arrays.sort(arr, compareBy<String> {
+        it.length
+    }.thenBy {
+        it.sumOf { it.digitToIntOrNull() ?: 0 }
+    }.thenBy {
+        it
+    })
     print(buildString {
         arr.forEach {
             appendLine(it)
