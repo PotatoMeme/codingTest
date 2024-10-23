ee786daa1461f000da5f86e894abff7d299a6ad1
KimSeongHwan <89847975+PotatoMeme@users.noreply.github.com>
2024-10-23 18:49:54 +0900

    [Bronze II] Title: 8진수, 10진수, 16진수, Time: 96 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Bronze/11816. 8진수， 10진수， 16진수/8진수， 10진수， 16진수.kt
+++ 백준/Bronze/11816. 8진수， 10진수， 16진수/8진수， 10진수， 16진수.kt
@@ -8,15 +8,22 @@ fun main() {
         var sum = 0
         if (input[1] == 'x') { // 16
             for (i in 2 until input.length) {
-                sum = (sum * 16) + (input[i].digitToInt(16))
+                sum = (sum shl 4) or (input[i].charToInt())
             }
         } else {//8
             for (i in 1 until input.length) {
-                sum = (sum shl 3) + (input[i] - '0')
+                sum = (sum shl 3) or (input[i] - '0')
             }
         }
         print(sum)
     } else {//10
         print(input)
     }
+}
+
+fun Char.charToInt(): Int {
+    if (this in '0'..'9') {
+        return this - '0'
+    }
+    return this - 'a' + 10
 }
